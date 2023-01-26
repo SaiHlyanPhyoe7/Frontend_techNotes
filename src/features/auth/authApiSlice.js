e91e5f968fc134bd9be980f718f9b6619c34cd9f
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-01-27 04:48:22 +0630

    5 18 50 done

---
+++ src/features/auth/authApiSlice.js
@@ -0,0 +1,46 @@
+import { apiSlice } from "../../app/api/apiSlice"
+import { logOut } from "./authSlice"
+
+export const authApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        login: builder.mutation({
+            query: credentials => ({
+                url: '/auth',
+                method: 'POST',
+                body: { ...credentials }
+            })
+        }),
+        sendLogout: builder.mutation({
+            query: () => ({
+                url: '/auth/logout',
+                method: 'POST',
+            }),
+            // rtk query support onQueryStarted
+            // we can call async func inside of endpoint
+            async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+                try {
+                    //const { data } = queryFulfilled
+                    await queryFulfilled
+                    //console.log(data)
+                    dispatch(logOut())
+                    // resetApiState it will clear out the cache and the query subscriptions and everything to do with our api slice
+                    dispatch(apiSlice.util.resetApiState())
+                } catch (err) {
+                    console.log(err)
+                }
+            }
+        }),
+        refresh: builder.mutation({
+            query: () => ({
+                url: '/auth/refresh',
+                method: 'GET',
+            })
+        }),
+    })
+})
+
+export const {
+    useLoginMutation,
+    useSendLogoutMutation,
+    useRefreshMutation,
+} = authApiSlice 
